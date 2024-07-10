376a71b0809f634fa5d26609afa6967c42b9f32f
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-10 14:19:19 +0100

    Role implements GrantedAutority

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,12 +1,13 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table (name="tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +24,8 @@ public class Role {
     public void setId(Long id) {
         this.id = id;
     }
-
+    //getAuthority
+    @Override
     public String getAuthority() {
         return authority;
     }
