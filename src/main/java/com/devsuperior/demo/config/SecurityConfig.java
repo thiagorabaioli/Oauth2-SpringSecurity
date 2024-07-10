dc32f4c4173b4a1069a9dcbe02f51e0f061cb677
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-10 13:48:49 +0100

    BcryptPasswordEncoder - Bean

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+   //Compoentes que podem ser injetados como métodos.
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder(){
+            return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
